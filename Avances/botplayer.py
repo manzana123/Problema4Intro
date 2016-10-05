c7314799174ab2b79c1a04e14d6a631dcc3c8760
fsantand <federicosantander95@gmail.com>
2016-10-04 21:40:41 -0300

    Carpeta de avances

---
+++ Avances/botplayer.py
@@ -0,0 +1,12 @@
+import numpy
+
+
+def escoger_movimiento( amenazas ):
+    movimiento_x = ""
+    movimiento_y = "" 
+    return movimiento_x + "," + movimiento_y
+
+def escoger_disparo( amenazas ):
+    disparo_x = ""
+    disparo_y = ""
+    return disparo_x + "," + disparo_y
